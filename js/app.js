e0302a8b39123a8823cd0598bafa2ef849aec21f
BranDanny <zhangdeng1010@163.com>
2017-06-28 23:36:55 +0800

    Add files via upload
    After review, and modify the code.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
-// 这是我们的玩家要躲避的敌人 
+//Magic Number
+var CELL_WIDTH = 101;
+var CELL_HEIGHT = 83;
+var OFFSET = 55;
+
+// 这是我们的玩家要躲避的敌人
 var Enemy = function (x,y,speed) {
   // 要应用到每个敌人的实例的变量写在这里
   this.x = x;
   this.y = y;
   this.speed = speed;
   // 我们已经提供了一个来帮助你实现更多
-
   // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
   this.sprite = 'images/enemy-bug.png';
 };
@@ -17,10 +21,9 @@ Enemy.prototype.update = function (dt) {
   // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
   // 都是以同样的速度运行的
   this.checkCollisions(player);
-  if(this.x >= 505){ //实现Enemy循环出现
-    this.x = -101;
+  if(this.x >= (5*CELL_WIDTH)){ //实现Enemy循环出现
+    this.x = -(CELL_WIDTH);
   }
-
 };
 
 // 此为游戏必须的函数，用来在屏幕上画出敌人，
@@ -32,10 +35,9 @@ Enemy.prototype.checkCollisions = function (player) {
   var xx = Math.min(this.x+71, player.x+101) - Math.max(this.x, player.x);
   var yy = Math.min(this.y+137, player.y+141) - Math.max(this.y+74, player.y+60);
   if(xx > 0 && yy > 0){ //碰撞后player回到初始位置
-    player.x = 202;
-    player.y = 387;
+    player.reset();
   }
-}
+};
 
 // 现在实现你自己的玩家类
 var Player = function (x,y) {
@@ -43,16 +45,20 @@ var Player = function (x,y) {
   this.y = y;
   this.sprite ='images/char-horn-girl.png'
 };
-// 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
+
 //分数统计
 var sc = 0;
 document.getElementById("score").innerHTML = String(sc);
 
+// 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数 和一个重置reset()函数
+Player.prototype.reset = function () {
+  this.x = (2*CELL_WIDTH);
+  this.y = (4*CELL_HEIGHT + OFFSET);
+};
+
 Player.prototype.update = function () {
   if(this.y == -28){ // 达到胜利条件, player回到初始位置
-    player.x = 202;
-    player.y = 387;
-    //this.gamePoint();
+    this.reset();
     sc += 100;
     document.getElementById("score").innerHTML = String(sc);//更新分数
   }
@@ -61,25 +67,25 @@ Player.prototype.update = function () {
 Player.prototype.render = function () {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y)
 };
+
 Player.prototype.handleInput = function (movement) {
   switch (movement) {
     //限制player移动范围
-    case 'left': if(this.x > 0){this.x -= 101};break;
-    case 'right': if(this.x < 404){this.x += 101};break;
-    case 'up': if(this.y > -28){this.y -= 83};break;
-    case 'down': if(this.y < 387){this.y += 83};break;
+    case 'left': if(this.x > 0){this.x -= CELL_WIDTH} break;
+    case 'right': if(this.x < 404){this.x += CELL_WIDTH} break;
+    case 'up': if(this.y > -28){this.y -= CELL_HEIGHT} break;
+    case 'down': if(this.y < 387){this.y += CELL_HEIGHT} break;
   }
 };
 
 // 现在实例化你的所有对象
 // 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
 var allEnemies = [];
-for(i=0;i<=3;i++){
-  allEnemies[i] = new Enemy(-101,83*(Math.floor(Math.random()*3))+55,Math.floor(Math.random()*(400-100+1)+100))
-};
+for(var i=0;i<=3;i++){
+  allEnemies[i] = new Enemy(-101,83*(Math.floor(Math.random()*3))+OFFSET,Math.floor(Math.random()*(400-100+1)+100))
+}
 // 把玩家对象放进一个叫 player 的变量里面
-var player = new Player(202,83*4+55);
-
+var player = new Player((2*CELL_WIDTH),(4*CELL_HEIGHT + OFFSET));
 
 // 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
 // 方法里面。你不需要再更改这段代码了。
@@ -90,6 +96,5 @@ document.addEventListener('keyup', function (e) {
     39: 'right',
     40: 'down'
   };
-
   player.handleInput(allowedKeys[e.keyCode]);
 });
